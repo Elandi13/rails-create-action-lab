a8ba44fc81bac6a9d01397f170eab85c02daa328
Elvis Landi <elandi13@yahoo.com>
2021-01-20 11:40:39 -0500

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,29 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new 
   end
 
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
   end
+  
+  
+  # def create
+  #   new_student = Student.create(student_params)
+  #   redirect_to student_path(new_student)
+  # end
+
+  # private
+  
+  # def student_params
+  #   params.require(:student).permit(:first_name, :last_name)
+  # end
 
 end
